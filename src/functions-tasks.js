fc85903207e9a8f5bcaed2ff86cd3065b3ec2c95
kaliganoff <kaliganoff@yahoo.com>
2024-04-11 15:04:50 +0300

    feat: Solve the last task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -222,8 +222,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let count = startFrom;
+  return function generator() {
+    const prevCount = count;
+    count += 1;
+    return prevCount;
+  };
 }
 
 module.exports = {
